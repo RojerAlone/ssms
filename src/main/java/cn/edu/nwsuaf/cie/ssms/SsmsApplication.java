b245cb1afc2f67e6fd560c61fa2ddd7e5fc71476
zhangrenjie <zhangrenjie@xiaomi.com>
2017-11-28 14:35:53 +0800

    create original SpringBoot

---
+++ src/main/java/cn/edu/nwsuaf/cie/ssms/SsmsApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nwsuaf.cie.ssms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmsApplication.class, args);
+	}
+}
