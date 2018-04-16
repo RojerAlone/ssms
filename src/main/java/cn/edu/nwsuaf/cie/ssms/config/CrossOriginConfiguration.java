0d6a24cf6e18a46965b61f3acbfdedf5f1247900
rojeralone <1091165843@qq.com>
2018-04-16 15:46:00 +0800

    support cross origin

---
+++ src/main/java/cn/edu/nwsuaf/cie/ssms/config/CrossOriginConfiguration.java
@@ -0,0 +1,16 @@
+package cn.edu.nwsuaf.cie.ssms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CrossOriginConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
